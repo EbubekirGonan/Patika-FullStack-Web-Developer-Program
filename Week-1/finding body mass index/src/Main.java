37bae6861a9ca57552d6f07935050d512000991a
Ebubekir Gonan <ebbkrgnn@gmail.com>
2023-12-12 15:32:04 +0300

    add some comments and readme file

--- Week-1/finding body mass index/src/Main.java
+++ Week-1/finding body mass index/src/Main.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double a, b, bmi;
 
+        //declare variables
+        double a, b, bmi;
+        //bmi = body mass index
+
+        //call scanner class
         Scanner input = new Scanner(System.in);
 
+        //demand for values
         System.out.print("Please enter your height: ");
         a = input.nextDouble();
 
         System.out.print("Please enter your weight: ");
         b = input.nextDouble();
 
+        //find bmi result
         bmi = b / (a * a);
         System.out.print("Your body mass index: " + bmi);
 

269e7b06a951d9f22371317b6f336aca202a5bcb
Ebubekir Gonan <ebbkrgnn@gmail.com>
2023-12-13 18:50:42 +0300

    add some comments lines

--- Week-1/user_login/src/Main.java
+++ Week-1/user_login/src/Main.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        //declare variables
         String userName, password, newPassword;
         int select;
 
+        //call scanner class
         Scanner input = new Scanner(System.in);
 
+        //demand for ID and Password
         System.out.print("ID: ");
         userName = input.nextLine();
         System.out.print("Password: ");
         password = input.nextLine();
 
-
+        //if else structure comparing id and password
         if (userName.equals("patika") && password.equals("java123")) {
             System.out.print("Entrance successful... ");
         } else if (!userName.equals("patika")) {
@@ -20,6 +22,7 @@ public class Main {
         } else if (!password.equals("java123")) {
             System.out.println("Password is wrong. Do you want to create new password?\nPress 1 to yes\nPress 2 to no");
 
+            //switch case structure for create new password
             select = input.nextInt();
             switch (select) {
                 case 1:

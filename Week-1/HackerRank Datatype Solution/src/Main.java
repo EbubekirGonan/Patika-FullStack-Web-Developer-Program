b713ce29cfd5caff26f239747224df3c95ad313a
Ebubekir Gonan <ebbkrgnn@gmail.com>
2023-12-15 01:57:50 +0300

    hackerrank problem solved

--- Week-1/HackerRank Datatype Solution/src/Main.java
+++ Week-1/HackerRank Datatype Solution/src/Main.java
@@ -13,36 +13,34 @@ class Solution{
 
             try
             {
-                long x=sc.nextLong();
+                long x= sc.nextLong();
                 System.out.println(x + " can be fitted in:");
-                if ((x > -128) && (128 >= x)) {
+                if ((x >= -128) && (128 >= x)) {
                     System.out.println("* byte");
                 }
-                if (x > -32768 && x <= 32767) {
+                if (x >= -32768 && x <= 32767) {
                     System.out.println("* short");
                 }
-                if (x > Math.pow(-2, 31) && x <= (Math.pow(2,31) - 1)) {
+                if (x >= Math.pow(-2, 31) && x <= (Math.pow(2,31) - 1)) {
                     System.out.println("* int");
                 }
-                if (x > Math.pow(-2, 63) && x <= (Math.pow(2, 63) - 1)) {
+                if (x >= Math.pow(-2, 63) && x <= (Math.pow(2, 63) - 1)) {
                     System.out.println("* long");
                 }
 
             }
             catch(Exception e)
             {
-                System.out.println(sc.nextInt()+" can't be fitted anywhere.");
+                System.out.println(sc.next()+" can't be fitted anywhere.");
             }
 
         }
     }
 }
 
-
 /*
 byte -128 to 127
 short -32,768 -to 32,767
 ınt -2,147,483,648 to 2,147,483,647
 long –9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
-
 */

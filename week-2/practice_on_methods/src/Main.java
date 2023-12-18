d90850e56dd962fcefd657489cac4571000a6ece
Ebubekir Gonan <ebbkrgnn@gmail.com>
2023-12-19 00:37:38 +0300

    add some comments and readme file

--- week-2/practice_on_methods/src/Main.java
+++ week-2/practice_on_methods/src/Main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 public class Main {
+
+    //2 fonksiyon tanımlandı. f ve g fonksiyonları.
+    //f fonksiyonu sayı 0 ya da negatif olana kadar sayısı 5 azaltır ve g fonksiyonunu çağırır.
+    //g fonksiyonu 2 parametre tutar. ilk f fonksiyonundan aldığı, ikincisi kullanıcının girdiği değerler.
+    //g fonksiyonu ilk değeri bulana kadar sayıya 5 ekler.
     static int f(int n){
         if (n<= 0){
             return n;
@@ -21,7 +26,9 @@ public class Main {
         System.out.print("Please enter a number: ");
         int n = inp.nextInt();
 
+        //burada f fonkisoyunundan gelen değer g fonksiyonuna aktarılır
         int j =f(n);
+        //g; hem j'yi hem de n'i tutar.
         g(j, n);
     }
 }

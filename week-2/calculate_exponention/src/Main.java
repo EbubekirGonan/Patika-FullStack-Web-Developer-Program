af6e896857df19a875f2fecf1a141840592e874a
Ebubekir Gonan <ebbkrgnn@gmail.com>
2023-12-18 18:43:38 +0300

    add calculate exponention method

---
+++ week-2/calculate_exponention/src/Main.java
@@ -0,0 +1,24 @@
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+public class Main {
+    
+    //Description method
+    static int expo(int b,int n){
+        int result=1;
+        for(int i=1; i<=n; i++){
+            result *= b;
+        }
+        return result;
+    }
+    //main method
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        //demand for base and exponent
+        System.out.print("Please enter base: ");
+        int b = inp.nextInt();
+        System.out.print("And exponent: ");
+        int n = inp.nextInt();
+        //print result
+        System.out.print(b + " to the " + n + "(th): " + expo(b, n));
+    }
+}

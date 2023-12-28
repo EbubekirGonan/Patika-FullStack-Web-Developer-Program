200509a5ab1ae1b3f155901efc23a8791c305a7d
Ebubekir Gonan <ebbkrgnn@gmail.com>
2023-12-28 15:36:05 +0300

    add some comments

--- week-2/advanced_calculater/src/Main.java
+++ week-2/advanced_calculater/src/Main.java
@@ -20,7 +20,7 @@ public class Main {
         return result;
     }
 
-    static double div(int a, int b) {1
+    static double div(int a, int b) {
         int result = a / b;
         System.out.println("Ratio is: " + result);
         return result;

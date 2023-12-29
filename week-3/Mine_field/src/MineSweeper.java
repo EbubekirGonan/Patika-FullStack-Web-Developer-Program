1bc4a160990c1db1ce32c45f0825481219e019f3
Ebubekir Gonan <ebbkrgnn@gmail.com>
2023-12-29 22:44:09 +0300

    add comments lines and readme file

--- week-3/Mine_field/src/MineSweeper.java
+++ week-3/Mine_field/src/MineSweeper.java
@@ -1,83 +1,113 @@
 import java.util.Scanner;
 
-
+//Değerlendirme formu 5
 public class MineSweeper {
+    //Değerlendirme formu 1
+    //minesweeper class variables are defined.
+    //Değerlendirme formu 2
     int width, height;
     int numOfMine;
     int numOfCells;
+    //Değerlendirme formu 3
+    //call scanner class
     Scanner inp = new Scanner(System.in);
 
+    //run method
     void run() {
-
+        //Değerlendirme formu 4
+        // get dimension from user
         setDimensions();
+
+        //create empty map named in "map"
         String[][] map = createEmptyMap();
+        //and then mine assing
         mineAssign(map);
 
+        //create second map called "player map"
         String[][] playerMap = createEmptyMap();
         printMatris(playerMap);
+        //just break
         System.out.println();
 
         int numOfSafeCells = (height * width) - numOfMine;
-
+        //win check control through safe cells
         boolean isGameOn = true;
-
         while (isGameOn) {
-            int x, y;
-
+            int targetX, targetY;
+            //get target coordinate from user
+            //Değerlendirme formu 9
             System.out.print("Hedef x kordinati: ");
-            x = inp.nextInt();
+            targetX = inp.nextInt();
             System.out.print("Hedef y kordinati: ");
-            y = inp.nextInt();
-
-            if (!isInsideMap(x, y)) {
+            targetY = inp.nextInt();
+            //if target is ofside the map, warning
+            //Değerlendirme formu 10
+            if (!isInsideMap(targetX, targetY)) {
                 System.out.println("Tablo dışına çıktınız. Lütfen geçerli koordinat giriniz: ");
                 continue;
             }
-            if (!playerMap[x][y].equals("- ") && !playerMap[x][y].equals("* ")) {
+            //or if user choose any already chosen one
+            if (!playerMap[targetX][targetY].equals("- ") && !playerMap[targetX][targetY].equals("* ")) {
                 System.out.println("Bu koordinat daha önce seçildi, başka bir koordinat girin");
                 continue;
             }
-
-            if (map[x][y].equals("* ")) {
-                System.out.println("Patladınız. Tekrar deneyin.");
-
-                // set mines to player map in order to show end result to player
-                for (int i = 0; i < map.length; i++) {
-                    for (int j = 0; j < map[i].length; j++) {
-                        if (map[i][j].equals("* ")) {
-                            playerMap[i][j] = map[i][j];
-                        }
-                    }
-                }
-                printMatris(playerMap);
-
+            //if users target is mine, game over.
+            if (map[targetX][targetY].equals("* ")) {
+                //Değerlendirme formu 13
+                loseGame();
                 isGameOn = false;
-                break;
+            }else {
+                //Değerlendirme formu 11
+                numOfSafeCells--;
+                // set number of neighbor mines to target coordinates and print
+                playerMap[targetX][targetY] = Integer.toString(getNumberOfNeighborMines(targetX, targetY, map)) + " ";
+                //Değerlendirme formu 12
+                printMatris(playerMap);
             }
-            numOfSafeCells--;
-
-            int numOfNeighborMines = getNumberOfNeighborMines(x, y, map);
-
-            playerMap[x][y] = Integer.toString(numOfNeighborMines) + " ";
 
             if (numOfSafeCells == 0) {
-                System.out.println("Tebrikler kazandınız.");
-                for (int i = 0; i < playerMap.length; i++) {
-                    for (int j = 0; j < playerMap[i].length; j++) {
-                        if (playerMap[i][j].equals("- ")) {
-                            playerMap[i][j] = "* ";
-                        }
-                    }
-                }
+                //if safe cells are over, win.
+                //Değerlendirme formu 14
+                winGame();
                 isGameOn = false;
             }
-            printMatris(playerMap);
+
+
+        }
+        // set mines to player map in order to show end result to player
+        setMinesToPlayerMap(map, playerMap);
+        printMatris(playerMap);
+    }
+
+    //win game method
+    //Değerlendirme formu 6
+    void winGame() {
+        System.out.println("Tebrikler kazandınız.");
+        //Değerlendirme formu 15
+    }
+
+    //lose game method
+    void loseGame(){
+        System.out.println("Patladınız. Tekrar deneyin.");
+    }
+
+    //set mines to player map method
+    //Değerlendirme formu 8
+    void setMinesToPlayerMap(String[][] map, String[][] playerMap){
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j].equals("* ")) {
+                    playerMap[i][j] = map[i][j];
+                }
+            }
         }
     }
 
+    //get number of neighbor mines method
     int getNumberOfNeighborMines(int x, int y, String[][] map) {
         int numOfNeighborMines = 0;
 
+        //there are 8 way to check nearby mines
         int[][] coordinatesToVisit = {
                 {x, y + 1},
                 {x + 1, y + 1},
@@ -89,6 +119,8 @@ public class MineSweeper {
                 {x - 1, y + 1},
         };
 
+        //each of x and y coordinates checking here but provided that it is not off the map
+        //then mines increase one by one
         for (int i = 0; i < coordinatesToVisit.length; i++) {
             int targetX = coordinatesToVisit[i][0];
             int targetY = coordinatesToVisit[i][1];
@@ -101,6 +133,7 @@ public class MineSweeper {
         return numOfNeighborMines;
     }
 
+    //check is inside map
     boolean isInsideMap(int x, int y) {
         if (x < 0 || y < 0) {
             return false;
@@ -112,6 +145,7 @@ public class MineSweeper {
     }
 
 
+    //print matris method
     void printMatris(String[][] matris) {
         for (int i = 0; i < matris.length; i++) {
             for (int j = 0; j < matris[i].length; j++) {
@@ -121,7 +155,8 @@ public class MineSweeper {
         }
     }
 
-
+    //set dimension method
+    //Değerlendirme formu 7
     void setDimensions() {
 
         System.out.print("Tablonun genişliğini giriniz: ");
@@ -138,24 +173,26 @@ public class MineSweeper {
         }
     }
 
+    //create empty map
     String[][] createEmptyMap() {
 
         String[][] map = new String[height][width];
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                map[i][j] = "- ";
+                map[i][j] = "- "; //fill map "- "
             }
         }
         return map;
     }
 
 
+    //mine assign method
     public String[][] mineAssign(String[][] map) {
         int remainingMineNumber = numOfMine;
         while (remainingMineNumber > 0) {
             int a = (int) (Math.random() * height);
             int b = (int) (Math.random() * width);
-            if (!map[a][b].equals("* ")) {
+            if (!map[a][b].equals("* ")) { //to prevent repetition
                 map[a][b] = "* ";
                 remainingMineNumber--;
             }

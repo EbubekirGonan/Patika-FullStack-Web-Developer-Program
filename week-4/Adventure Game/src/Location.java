a7440f545b3daf34c5dab2c20de89f1fc82737b2
Ebubekir Gonan <ebbkrgnn@gmail.com>
2024-01-08 22:54:30 +0300

    change just folder name

---
+++ week-4/Adventure Game/src/Location.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public abstract class Location {
+    private Player player;
+    private String name;
+    public static Scanner input = new Scanner(System.in);
+    protected boolean isCompleted = false;
+
+    public Location(Player player, String name) {
+        this.player = player;
+        this.name = name;
+    }
+    public abstract boolean visit();
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public boolean isCompleted(){
+        return this.isCompleted;
+    }
+}
---
+++ week-4/Adventure Game/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //the main method where the game starts with game start method
+        Game game = new Game();
+        game.start();
+    }
+}

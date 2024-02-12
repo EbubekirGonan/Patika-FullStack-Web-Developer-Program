c4d938378bbcee84e38c286d82396a61b46999bd
Ebubekir Gonan <ebbkrgnn@gmail.com>
2024-02-12 13:36:05 +0300

    add rent a car app

---
+++ week-7/PATIKA_RENTACAR/src/App.java
@@ -0,0 +1,16 @@
+import business.UserManager;
+import core.Db;
+import core.Helper;
+import view.AdminView;
+import view.LoginView;
+
+import java.sql.Connection;
+
+public class App {
+    public static void main(String[] args) {
+
+        Helper.setTheme();
+        LoginView loginView = new LoginView();
+
+    }
+}
---
+++ week-7/PATIKA_RENTACAR/src/view/LoginView.java
@@ -0,0 +1,45 @@
+package view;
+
+import business.UserManager;
+import core.Helper;
+import entity.User;
+
+import javax.swing.*;
+
+public class LoginView extends Layout{
+    private JPanel container;
+    private JPanel w_top;
+    private JLabel lbl_welcome;
+    private JLabel lbl_welcome2;
+    private JPanel w_bottom;
+    private JTextField fld_username;
+    private JPasswordField fld_pass;
+    private JButton button_login;
+    private JLabel lbl_pass;
+    private JLabel lbl_username;
+    private final UserManager userManager;
+
+    public LoginView(){
+        this.userManager = new UserManager();
+        this.add(container);
+        this.guiInitialise(400,400);
+
+        button_login.addActionListener(e -> {
+            JTextField[] checkFieldList = {this.fld_username, this.fld_pass};
+            if(Helper.isFieldListEmpty(checkFieldList)){
+                Helper.showMsg("fill");
+            } else {
+                User loginUser = this.userManager.findByLogin(this.fld_username.getText(), this.fld_pass.getText());
+                if(loginUser == null){
+                    Helper.showMsg("notFound");
+                }else {
+                    AdminView adminView = new AdminView(loginUser);
+                    dispose();
+                }
+            }
+
+        });
+    }
+
+
+}

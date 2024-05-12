2e856a012471a497b415d274c28bee3f88485406
Ebubekir Gonan <ebbkrgnn@gmail.com>
2024-05-13 00:33:58 +0300

    add bill-gates app

--- frontend/week-8/bill-gates/bill-gates/src/App.jsx
+++ frontend/week-8/bill-gates/bill-gates/src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/Header';
+import Title from './components/Title'
+import MoneyBar from './components/MoneyBar';
+import ProductContainer from './components/ProductContainer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
+  const [totalMoney, setTotalMoney] = useState(100000000000)
+
+
+  useEffect(() => {
+    axios
+    .get("https://dummyjson.com/products")
+    .then((response) => {
+      setProducts(response.data.products);
+      console.log(products);
+    })
+   
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* header */}
+      <div className="page">
+
+        <Header/>
+
+        <div className="container">
+
+          <Title/>
+          <MoneyBar totalMoney={totalMoney} setTotalMoney={setTotalMoney}/>
+
+          <ProductContainer products={products} totalMoney={totalMoney} setTotalMoney={setTotalMoney} />
+
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+     
     </>
   )
 }
